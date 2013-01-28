39404b729364e32c68f79090156682ef581501e9
Conrad Barski <lisperati@gmail.com>
2013-01-28 13:00:09 -0600

    start codox docs

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject webfui "0.2.1"
             :description "Alpha release of Webfui- All Webfui source included in this repository release under Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)"
+            :dev-dependencies [[lein-autodoc "0.9.0"]]
             :url "http://lisperati.com")
